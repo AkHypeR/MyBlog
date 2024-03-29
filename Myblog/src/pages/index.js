6039f55ab59f28c21c26d7243e6e4e5251547bca
AkHyper <akashroy.9126@gmail.com>
2024-03-29 11:00:58 +0530

    blog

---
+++ Myblog/src/pages/index.js
@@ -0,0 +1,8 @@
+import AddPost from "./pages/AddPost";
+import Signup from './pages/Signup'
+import EditPost from "./pages/EditPost";
+import Home from './pages/Home.jsx'
+import Post from "./pages/Post";
+import Login from './Login.jsx'
+import AllPosts from './pages/AllPosts';
+export{AllPosts, EditPost, Post,Signup,AddPost,Login,Home} 
